bf4b37a65302055a98960b3c15d43bb5bbbbeed0
Matt Bise <bise.matthew.ext@siemens.com>
2019-04-23 13:24:19 -0400

    Add and configure Storybook

---
+++ src/transform/TransformDialog.stories.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import TransformDialog from "./TransformDialog";
+
+storiesOf("TransformDialog", module).add("with 1 transformed project", () => <div>Hello</div>);
