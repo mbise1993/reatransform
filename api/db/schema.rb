df6cc59aee12c14fe8382d17582c8fc72bedb7b2
Matt Bise <mbise1993@gmail.com>
2019-04-25 22:51:01 -0400

    Update User model

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_050445) do
+ActiveRecord::Schema.define(version: 2019_04_26_024255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
