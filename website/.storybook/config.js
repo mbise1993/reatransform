73b8474f002393799abc9ce3f11e2de78559f9aa
Matt Bise <mbise1993@gmail.com>
2019-04-29 21:45:02 -0400

    Use global provider decorator for Storybook

--- website/.storybook/config.js
+++ website/.storybook/config.js
@@ -1,7 +1,11 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+
+import { withProvider } from '../src/test/decorators';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../src/index.css';
+
+addDecorator(withProvider);
 
 const req = require.context('../src', true, /\.stories\.tsx$/);
 
--- website/src/users/components/AuthDialog.stories.tsx
+++ website/src/users/components/AuthDialog.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import { withProvider } from '../../test/decorators';
 import TestPanel from '../../test/TestPanel';
 import LoginForm from './LoginForm';
 import SignUpForm from './SignUpForm';
@@ -11,22 +10,16 @@ import AuthDialog from './AuthDialog';
 const width = '400px';
 const height = '300px';
 
-storiesOf('AuthDialog', module)
-  .addDecorator(withProvider)
-  .add('login form', () => (
-    <TestPanel width={width} height={height}>
-      <LoginForm onSignUpClick={action('sign up clicked')} />
-    </TestPanel>
-  ));
+storiesOf('AuthDialog', module).add('login form', () => (
+  <TestPanel width={width} height={height}>
+    <LoginForm onSignUpClick={action('sign up clicked')} />
+  </TestPanel>
+));
 
-storiesOf('AuthDialog', module)
-  .addDecorator(withProvider)
-  .add('sign up form', () => (
-    <TestPanel width={width} height={height}>
-      <SignUpForm onSubmit={action('submit clicked')} />
-    </TestPanel>
-  ));
+storiesOf('AuthDialog', module).add('sign up form', () => (
+  <TestPanel width={width} height={height}>
+    <SignUpForm onSubmit={action('submit clicked')} />
+  </TestPanel>
+));
 
-storiesOf('AuthDialog', module)
-  .addDecorator(withProvider)
-  .add('full dialog', () => <AuthDialog show={true} onClose={() => {}} />);
+storiesOf('AuthDialog', module).add('full dialog', () => <AuthDialog show={true} onClose={() => {}} />);
